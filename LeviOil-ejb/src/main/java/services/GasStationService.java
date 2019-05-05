3313346db309d808827b0e8dd6582a8c7bd06df5
samihlayeb <mohamedsamih.layeb@esprit.tn>
2019-05-05 18:17:17 +0100

    GasStation-xhtml

--- LeviOil-ejb/src/main/java/services/GasStationService.java
+++ LeviOil-ejb/src/main/java/services/GasStationService.java
@@ -1,11 +1,12 @@
 package services;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
+import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
+
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
@@ -13,6 +14,7 @@ import entity.GasStation;
 import interfaces.GasStationIservice;
 
 @Stateless 
+@LocalBean
 public class GasStationService implements GasStationIservice {
 	@PersistenceContext(unitName = "LeviOil-ejb") 
 	EntityManager em; 
@@ -54,9 +56,9 @@ public class GasStationService implements GasStationIservice {
 
 	
 	public List<GasStation> findAllGasStations() {
-		System.out.println("+++++: ");
+
 		List<GasStation> list = em.createQuery("from GasStation", GasStation.class).getResultList(); 
-		System.out.println("11111111111111111111111111"+list.size()); 
+		System.out.println(list.toString());
 		return list; 
 
     }
